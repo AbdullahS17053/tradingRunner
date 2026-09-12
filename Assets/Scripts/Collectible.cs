75bd541ff69d09632ca68c604d2618da82c1b48e
AdilDevStuff <adilshafiq784@gmail.com>
2026-09-12 19:44:32 +0500

    Add Coin Particles

--- Assets/Scripts/Collectible.cs
+++ Assets/Scripts/Collectible.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class Collectible : MonoBehaviour
 {
+    public ParticleSystem coinParticles;
+
+    public MeshRenderer coinRenderer;
+    public Collider coinCollider;
+
+    [Header("Particle Offsets")]
+    [SerializeField] private float spawnHeightOffset = 1.0f;
 
     private void OnTriggerEnter(Collider collider)
     {
         if (collider.CompareTag("Player"))
         {
+            if (coinCollider != null) coinCollider.enabled = false;
+
+            if (coinRenderer != null) coinRenderer.enabled = false;
+
+            Vector3 particlePos = coinParticles.transform.position;
+            particlePos.y += spawnHeightOffset;
+            coinParticles.transform.position = particlePos;
+
+            coinParticles.Play();
+
             GameManager.Instance.takeProfit += 1;
             SoundManager.Instance.PlaySFX(SoundManager.Instance.collectTPSound);
-            this.gameObject.SetActive(false);
+
+            StartCoroutine(DisableAfterEmissionComplete());
         }
+    }
+
+    private IEnumerator DisableAfterEmissionComplete()
+    {
+        float totalWaitTime = coinParticles.main.duration + coinParticles.main.startLifetime.constantMax;
+
+        yield return new WaitForSeconds(totalWaitTime);
+
+        if (coinRenderer != null) coinRenderer.enabled = true;
+        if (coinCollider != null) coinCollider.enabled = true;
+
+        this.gameObject.SetActive(false);
     }
 }
